f04b92f6a30bd86f3b973862b24da4beb2dcedc9
IvanSolo666 <137136882+IvanSolo666@users.noreply.github.com>
2023-06-20 17:44:10 +0500

    Add files via upload

---
+++ main.py (ред 20,06).py
@@ -0,0 +1,66 @@
+def in_print_1(q):
+
+    q = input("1 процент: ")
+    if int(q) == 1 :
+
+        print("1 процент")
+        print(type(q))
+        print(type(1))
+        print(type('1 процент'))
+
+
+
+def in_print_2(q):
+
+    q = input("2 процента: ")
+    if int(q) == 1 :
+
+        print("2 процента")
+        print(type(q))
+        print(type(1))
+        print(type('2 процента'))
+
+def in_print_3(h):
+
+        q = input("3 процента: ")
+        if int(q) == 1 :
+
+            print("3 процента")
+            print(type(q))
+            print(type(1))
+            print(type('3 процента'))
+
+
+def in_print_4(c):
+
+        q = input("4 процента: ")
+        if int(q) == 1 :
+
+            print("4 процента")
+            print(type(q))
+            print(type(1))
+            print(type('4 процента'))
+
+   
+def in_print_5(t):
+
+        q = input("5 процентов: ")
+        if int(q) == 1 :
+
+            print("5 процентов")
+            print(type(q))
+            print(type(1))
+            print(type('5 процентов'))
+
+        if __name__ == '__main__':
+         in_print_1("q")
+        in_print_2("k")
+        in_print_3('h')
+        in_print_4("c")
+        in_print_5("t")
+        
+
+    
+
+
+
