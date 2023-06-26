d1caab3211cfd51eaf0ac8a7222851f4ac9761ca
IvanSolo666 <137136882+IvanSolo666@users.noreply.github.com>
2023-06-26 17:48:17 +0500

    Add files via upload

---
+++ main.py (ред 26,06) мегахарош.py
@@ -0,0 +1,222 @@
+def in_print_1():
+ 
+    paul = True
+
+    while paul == True:
+
+        q = input("Введите число: ")
+        if 4 == 1 :
+            print("1 процент")
+            paul == True
+        
+        ho =' ' 
+        if int(q) == 1 :
+            print(f"{q} процент" +ho)
+            paul == True
+        
+        off ='а' 
+        if int(q) > 1 and int(q) <= 4 :
+            print(f"{q} процент" + off)
+            paul == True
+            
+            if q == 101:
+               print(type(q))
+            enddd = True
+           
+
+
+        on ='ов' 
+        if int(q) > 4 and int(q) <21:
+            print(f"{q} процент" + on)
+            paul == True
+            
+        ho =' ' 
+        if int(q) == 21 :
+            print(f"{q} процент" + ho)
+            paul == True
+
+        off ='а' 
+        if int(q) > 21 and int(q) <25:
+            print(f"{q} процент" + off)
+            paul == True
+
+        on ='ов' 
+        if int(q) >= 25 and int(q) <31:
+            print(f"{q} процент" + on)
+            paul == True
+             
+        ho =' ' 
+        if int(q) == 31 :
+            print(f"{q} процент" + ho)
+            paul == True
+        
+        off ='а' 
+        if int(q) > 31 and int(q) <35:
+            print(f"{q} процент" + off)
+            paul == True
+                    
+        on ='ов' 
+        if int(q) >= 35 and int(q) <41:
+            print(f"{q} процент" + on)
+            paul == True
+
+        ho =' ' 
+        if int(q) == 41:
+            print(f"{q} процент" + ho)
+            paul == True
+
+        off ='а' 
+        if int(q) > 41 and int(q) <45:
+            print(f"{q} процент" + off)
+            paul == True
+            
+        on ='ов' 
+        if int(q) >= 45 and int(q) <51:
+            print(f"{q} процент" + on)
+            paul == True
+
+        ho =' ' 
+        if int(q) == 51:
+            print(f"{q} процент" + ho)
+            paul == True
+
+        off ='а' 
+        if int(q) > 51 and int(q) <55:
+            print(f"{q} процент" + off)
+            paul == True
+
+        on ='ов' 
+        if int(q) >= 55 and int(q) <61:
+            print(f"{q} процент" + on)
+            paul == True
+
+        ho =' ' 
+        if int(q) == 61:
+            print(f"{q} процент" + ho)
+            paul == True
+
+        off ='а' 
+        if int(q) > 61 and int(q) <65:
+            print(f"{q} процент" + off)
+            paul == True
+
+        on ='ов' 
+        if int(q) >= 65 and int(q) <71:
+            print(f"{q} процент" + on)
+            paul == True
+
+        ho =' ' 
+        if int(q) == 71:
+            print(f"{q} процент" + ho)
+            paul == True
+
+        off ='а' 
+        if int(q) > 71 and int(q) <75:
+            print(f"{q} процент" + off)
+            paul == True
+
+        on ='ов' 
+        if int(q) >= 75 and int(q) <81:
+            print(f"{q} процент" + on)
+            paul == True
+
+        ho =' ' 
+        if int(q) == 81:
+            print(f"{q} процент" + ho)
+            paul == True
+
+        off ='а' 
+        if int(q) > 81 and int(q) <85:
+            print(f"{q} процент" + off)
+            paul == True
+
+        on ='ов' 
+        if int(q) >= 85 and int(q) <91:
+            print(f"{q} процент" + on)
+            paul == True
+
+        ho =' ' 
+        if int(q) == 91:
+            print(f"{q} процент" + ho)
+            paul == True
+
+        off ='а' 
+        if int(q) > 91 and int(q) <95:
+            print(f"{q} процент" + off)
+            paul == True
+
+        on ='ов' 
+        if int(q) >= 95 and int(q) <101:
+            print(f"{q} процент" + on)
+            paul == True
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+                        
+
+
+
+
+    if q == 101:
+            print(type(q))
+            enddd = True
+
+
+
+in_print_1()
+
+
+# while paul2 == True :
+
+#     q = input("1 процент: ")
+#     if int(q) == 1 :
+
+#                 print("2 процента")
+#                 print(type(q))
+#                 print(type(2))
+#                 print(type('2 процента'))
+
+#     if q == 2:
+#         print(type(q))
+#         endd = True
+
+
+
+
+
+
+# while paul2 == True :
+
+#     q = input("1 процент: ")
+#     if int(q) == 1 :
+
+#                 print("2 процента")
+#                 print(type(q))
+#                 print(type(2))
+#                 print(type('2 процента'))
+
+#     if q == 2:
+#         print(type(q))
+#         endd = True
+
+
